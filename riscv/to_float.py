0b5eec6833b66356a62bb040096acad9c4950132
Syed Taha <syetaha@gmail.com>
2025-05-12 22:02:56 +0500

    debugging changes

--- riscv/to_float.py
+++ riscv/to_float.py
@@ -1,7 +1,7 @@
 import struct
 
 # Input hex string
-hex_string = "beeb6d22beeb6d22beeb6d22beeb6d22beec8f54beff0825befa2ee4beff96e1"
+hex_string = "be8aa64cbdf4eb9abe585a4fbea6ed89bca95c85be5512a9bdf5ae1dbe5bca53"
 
 # Ensure hex string has a length that's a multiple of 8 characters (4 bytes)
 if len(hex_string) % 8 != 0:
