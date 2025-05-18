d8d794fa8bf0f2dcc4fa802bf452320ef0a60460
Syed Taha <syetaha@gmail.com>
2025-05-18 22:19:36 +0500

    fix: update usage in to_float_all.py for flatten instruction and shape

--- riscv/to_float_all.py
+++ riscv/to_float_all.py
@@ -50,15 +50,15 @@ def display_floats(values, shape=None, channel_view=False):
             print(f"{value:.3f}", end=" ")
         print()
 
-# Example usage
 if __name__ == "__main__":
-    filename = "riscv/build/logs/conv2d.txt"
-    instruction = "vfredsum"
+    filename = "riscv/build/logs/flatten.txt"
+    instruction = "vlse32.v v0, (s5), s2"
     
     values = extract_floats_from_log(filename, instruction=instruction, first_only=(instruction == "vfredsum"))
 
     display_floats(
         values,
-        shape=(8, 24, 24),              # e.g., (8, 24, 24) if reshaping
+        shape=(8, 12, 12),              # e.g., (8, 24, 24) if reshaping
+        # shape=None,              # e.g., (8, 24, 24) if reshaping
         channel_view=True       # Set True if using (C, H, W)
     )
