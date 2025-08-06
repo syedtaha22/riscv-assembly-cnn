0c4a3eda43fbbfb2eefd3cbcf2e127a38320e895
Syed Taha <143250416+syedtaha22@users.noreply.github.com>
2025-08-07 01:24:28 +0500

    feat: add IM2COL + GEMM implementation for 2D convolution

--- c_cpp/conv2d.h
+++ c_cpp/conv2d.h
@@ -46,4 +46,86 @@ void conv2d(float* input) {
     }
 }
 
+
+/**
+ * @brief Below is an optimised IM2COL + GEMM implementation for 2D convolution.
+ *
+ * It is written to minimize number of operations and memory accesses. By
+ * making use of pointer arithmetic.
+ *
+ * This code is commented out to avoid conflicts with the main conv2d function (above),
+ * which was used for initial benchmarks.
+ *
+ */
+
+
+ // #define PATCH_SIZE (CL_FILTER_DIM * CL_FILTER_DIM)
+ // #define NUM_PATCHES (CL_OUT_DIM * CL_OUT_DIM)
+ // #define CLS_CLINDM (CL_STRIDE * CL_IN_DIM)
+ // #define CLFD_NUMP (CL_FILTER_DIM * NUM_PATCHES)
+ //
+ // void im2col(const float* input, float* im2col_matrix) {
+ //     // im2col_matrix size: PATCH_SIZE * NUM_PATCHES
+ //     static const uint32_t odps = CL_OUT_DIM * PATCH_SIZE; // output dimension * patch size
+ //     static const uint32_t idcs = CL_IN_DIM * CL_STRIDE; // input dimension *  convolution stride
+ //
+ //     for (uint32_t i = 0; i < CL_OUT_DIM; ++i) {
+ //         uint32_t iodps = i * odps; // i = i, od = output dimension, ps = patch size
+ //         uint32_t iidcs = i * idcs;   // i = i, id = input dimension, cs = convolution stride
+ //
+ //         for (uint32_t j = 0; j < CL_OUT_DIM; ++j) {
+ //             uint32_t patch_col = j * PATCH_SIZE + iodps; // patch_col = i * CL_OUT_DIM + j
+ //             uint32_t input_row_base = j * CL_STRIDE + iidcs; // input
+ //
+ //             for (uint32_t fi = 0; fi < CL_FILTER_DIM; ++fi) {
+ //                 uint32_t input_row = fi * CL_IN_DIM + input_row_base;
+ //                 uint32_t patch_base = fi * CL_FILTER_DIM + patch_col;
+ //
+ //                 // uint32_t patch_row_base = fi * CL_FILTER_DIM;
+ //                 float vals[CL_FILTER_DIM]; // vle16, vse16
+ //                 for (uint32_t fj = 0; fj < CL_FILTER_DIM; ++fj) vals[fj] = input[input_row + fj];
+ //                 for (uint32_t fj = 0; fj < CL_FILTER_DIM; ++fj) im2col_matrix[patch_base + fj] = vals[fj];
+ //             }
+ //
+ //         }
+ //     }
+ // }
+ //
+ // void conv2d(float* input) {
+ //     // Allocate im2col matrix as 1D array
+ //     float im2col_matrix[PATCH_SIZE * NUM_PATCHES];
+ //
+ //     // Fill im2col matrix
+ //     im2col(input, im2col_matrix);
+ //
+ //     float* bptr = conv_biases; // Pointer to the bias for the current filter
+ //     float* optr = conv_out; // Pointer to the output array
+ //     float* fptr = conv_filters; // Pointer to the current filter weights
+ //
+ //     // GEMM: output = filters (CL_NUM_FILTERS x PATCH_SIZE) * im2col_matrix (PATCH_SIZE x NUM_PATCHES)
+ //     for (uint32_t f = 0; f < CL_NUM_FILTERS; ++f) {
+ //         float* iptr = im2col_matrix; // Pointer to the im2col matrix
+ //
+ //         // Loop Over all patches
+ //         for (uint32_t p = 0; p < NUM_PATCHES; ++p) {
+ //             float sum = *bptr; // Start with the bias for the current filter
+ //             float* tfptr = fptr; // Pointer to the current filter weights
+ //
+ //             uint32_t temp = PATCH_SIZE; // Number of elements in the filter
+ //
+ //             while (temp) {
+ //                 temp--; // Decrement the number of elements left to process
+ //                 sum += *tfptr * *iptr; // Element-wise multiplication and accumulation
+ //                 tfptr++; // Move to the next filter weight
+ //                 iptr++; // Move to the next element in im2col matrix
+ //             }
+ //
+ //             *optr = sum; // Store the result in the output
+ //             optr++; // Move to the next output element
+ //         }
+ //         fptr += PATCH_SIZE; // Move to the next filter weights
+ //         bptr++;
+ //     }
+ // }
+
 #endif // CONV2D_H
